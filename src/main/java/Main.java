fc088db0cb21c7705e743edf807e86852f432237
cedricplouvier <cedric.plouvier@hotmail.com>
2022-12-02 15:24:16 +0100

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,14 +16,289 @@ import java.io.IOException;
 import java.net.URL;
 
 public class Main {
+    static Pattern abraBack = new Pattern("src/main/resources/img/abraBack.png").similar((float) 0.8);
+    static Pattern abraLeft = new Pattern("src/main/resources/img/abraLeft.png").similar((float) 0.8);
+    static Pattern abraRight = new Pattern("src/main/resources/img/abraRight.png").similar((float) 0.8);
+    static Pattern abraFront = new Pattern("src/main/resources/img/abraFront.png").similar((float) 0.8);
+    static Pattern abraFight = new Pattern("src/main/resources/img/abraFight.png").similar((float) 0.8);
+    static Pattern fermerCombat = new Pattern("src/main/resources/img/fermerCombat.png").similar((float) 0.8);
+
+    static Pattern options = new Pattern("src/main/resources/img/options.png").similar((float) 0.78);
+    static Pattern tropCharge = new Pattern("src/main/resources/img/tropCharge.png").similar((float) 0.80);
+    static Pattern popoRappel = new Pattern("src/main/resources/img/popoRappel.png").similar((float) 0.80);
+    static Pattern popoBonta = new Pattern("src/main/resources/img/popoBonta.png").similar((float) 0.80);
+    static Pattern entreMaison = new Pattern("src/main/resources/img/entreMaison.png").similar((float) 0.80);
+    static Pattern One = new Pattern("src/main/resources/img/one.png").similar((float) 0.80);
+    static Pattern Nine = new Pattern("src/main/resources/img/nine.png").similar((float) 0.80);
+    static Pattern Zero = new Pattern("src/main/resources/img/zero.png").similar((float) 0.80);
+    static Pattern Seven = new Pattern("src/main/resources/img/seven.png").similar((float) 0.80);
+    static Pattern Six = new Pattern("src/main/resources/img/six.png").similar((float) 0.80);
+    static Pattern delock = new Pattern("src/main/resources/img/delock.png").similar((float) 0.80);
+    static Pattern ouvreCoffre = new Pattern("src/main/resources/img/ouvreCoffre.png").similar((float) 0.80);
+    static Pattern ressourceInventaire = new Pattern("src/main/resources/img/ressourceInventaire.png").similar((float) 0.80);
+    static Pattern bworky = new Pattern("src/main/resources/img/bworky.png").similar((float) 0.80);
+    static Pattern arrowInventaire = new Pattern("src/main/resources/img/arrowInventaire.png").similar((float) 0.80);
+    static Pattern familierInventaire = new Pattern("src/main/resources/img/familierInventaire.png").similar((float) 0.80);
+    static Pattern swordInventaire = new Pattern("src/main/resources/img/swordInventaire.png").similar((float) 0.80);
+    static Pattern max = new Pattern("src/main/resources/img/max.png").similar((float) 0.80);
+    static Pattern check = new Pattern("src/main/resources/img/check.png").similar((float) 0.80);
+    static Pattern close = new Pattern("src/main/resources/img/close.png").similar((float) 0.80);
+    static Pattern zaapChateau = new Pattern("src/main/resources/img/zaapChateau.png").similar((float) 0.80);
+    static Pattern zaapAstrub = new Pattern("src/main/resources/img/zaapAstrub.png").similar((float) 0.80);
+    static Pattern utiliser = new Pattern("src/main/resources/img/utiliser.png").similar((float) 0.80);
+    static Pattern selectZaapViVi = new Pattern("src/main/resources/img/selectZaapViVi.png").similar((float) 0.80);
+    static Pattern selectZaapMili = new Pattern("src/main/resources/img/selectZaapMili.png").similar((float) 0.80);
+    static Pattern oui = new Pattern("src/main/resources/img/oui.png").similar((float) 0.80);
+    static Pattern pykur = new Pattern("src/main/resources/img/pykur.png").similar((float) 0.80);
+    static Pattern nomoon = new Pattern("src/main/resources/img/nomoon.png").similar((float) 0.80);
+
+    static Pattern ambre = new Pattern("src/main/resources/img/ambre.png").similar((float) 0.80);
+    static Pattern ambreAbra = new Pattern("src/main/resources/img/ambreAbra.png").similar((float) 0.80);
+    static Pattern boisEnvoute = new Pattern("src/main/resources/img/boisEnvoute.png").similar((float) 0.80);
+    static Pattern bourgeonAbra = new Pattern("src/main/resources/img/bourgeonAbra.png").similar((float) 0.80);
+    static Pattern clefAbra = new Pattern("src/main/resources/img/clefAbra.png").similar((float) 0.80);
+    static Pattern racineAbra = new Pattern("src/main/resources/img/racineAbra.png").similar((float) 0.80);
+    static Pattern seveAbra = new Pattern("src/main/resources/img/seveAbra.png").similar((float) 0.80);
+    static Pattern abraGlands = new Pattern("src/main/resources/img/abraGlands.png").similar((float) 0.80);
+    static Pattern ecorceAbra = new Pattern("src/main/resources/img/ecorceAbra.png").similar((float) 0.80);
 
     public static void main(String[] args) throws IOException, InterruptedException, AWTException, FindFailed {
-        //abraglands();
+        abraglandsIop();
         //betaKamas();
         //arakne();
-        cheminFer();
+        //cheminFer();
+        //depodsGlands();
     }
 
+
+    public static void abraglandsIop() throws InterruptedException, FindFailed {
+
+        TimeUnit.SECONDS.sleep(3);
+        Keyboard kb = new DesktopKeyboard();
+        Screen s = new Screen();
+
+        TimeUnit.SECONDS.sleep(3);
+        for (int i = 0; i < 340; i++) {
+            try {
+                boolean abraFound = false;
+                TimeUnit.SECONDS.sleep(1);
+                s.type(Key.F2);
+                TimeUnit.SECONDS.sleep(1);
+                if (s.exists(abraBack) != null) {
+                    System.out.println("abraback");
+                    abraFound = true;
+                    s.rightClick(abraBack);
+                } else if (s.exists(abraLeft) != null) {
+                    System.out.println("abraleft");
+                    abraFound = true;
+                    s.rightClick(abraLeft);
+                } else if (s.exists(abraRight) != null) {
+                    System.out.println("abraright");
+                    abraFound = true;
+                    s.rightClick(abraRight);
+                } else if (s.exists(abraFront) != null) {
+                    System.out.println("abrafront");
+                    abraFound = true;
+                    s.rightClick(abraFront);
+                } else {
+                    System.out.println("ABRA NOT FOUND");
+                }
+                if(abraFound) {
+                    TimeUnit.SECONDS.sleep(1);
+                    Location A = new Location(735, 410);
+                    s.click(A);
+                    TimeUnit.SECONDS.sleep(1);
+                    Location B = new Location(900, 350);
+                    s.click(B);
+
+                    //TimeUnit.SECONDS.sleep(5); //temps que enus rejoignes
+                    s.type(Key.F1);
+
+                    TimeUnit.SECONDS.sleep(2);
+                    s.type(Key.F7);
+                    s.click(abraFight);
+
+                    TimeUnit.SECONDS.sleep(1);
+                    s.type(Key.F7);
+                    s.click(abraFight);
+
+                    TimeUnit.SECONDS.sleep(1);
+                    s.type(Key.F7);
+                    s.click(abraFight);
+
+                    TimeUnit.SECONDS.sleep(2);
+                    while (s.exists(abraFight) != null) {
+                        s.type(Key.F6);
+                        s.click(abraFight);
+                        System.out.println("abraFight");
+                        TimeUnit.SECONDS.sleep(1);
+                        s.type(Key.F6);
+                        s.click(abraFight);
+                        TimeUnit.SECONDS.sleep(1);
+                        s.type(Key.F1);
+                        TimeUnit.SECONDS.sleep(10);
+                    }
+                }
+            } catch (FindFailed e) {
+                e.printStackTrace();
+            }
+            if (s.exists(fermerCombat) != null) {
+                s.click(fermerCombat);
+            }
+        }
+
+        TimeUnit.SECONDS.sleep(1);
+        s.doubleClick(popoRappel);
+        TimeUnit.SECONDS.sleep(5);
+        Location D = new Location(860, 60);
+        s.click(D);
+        TimeUnit.SECONDS.sleep(5);
+        Location AA = new Location(1245, 350);
+        s.click(AA);
+        TimeUnit.SECONDS.sleep(5);
+        Location BB = new Location(490, 135);
+        s.click(BB);
+        TimeUnit.SECONDS.sleep(2);
+        s.click(entreMaison);
+        TimeUnit.SECONDS.sleep(3);
+        s.click(One);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(Nine);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(Zero);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(Seven);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(delock);
+        TimeUnit.SECONDS.sleep(1);
+        Location E = new Location(940, 370);
+        s.click(E);
+        TimeUnit.SECONDS.sleep(5);
+        Location F = new Location(740, 440);
+        s.click(F);
+        s.click(ouvreCoffre);
+        TimeUnit.SECONDS.sleep(3);
+        s.click(One);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(Nine);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(Zero);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(Seven);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(delock);
+        TimeUnit.SECONDS.sleep(3);
+        Location G = new Location(1100, 200);
+        s.click(G);
+        TimeUnit.SECONDS.sleep(1);
+        Location H = new Location(370, 200);
+        s.click(H);
+        TimeUnit.SECONDS.sleep(1);
+        Match region1 = s.find(ambre);
+        Location I = new Location(500, 470);
+        s.dragDrop(region1, I);
+        s.click(max);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(check);
+        TimeUnit.SECONDS.sleep(1);
+        Match region2 = s.find(ambreAbra);
+        s.dragDrop(region2, I);
+        s.click(max);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(check);
+        TimeUnit.SECONDS.sleep(1);
+        Match region3 = s.find(bourgeonAbra);
+        s.dragDrop(region3, I);
+        s.click(max);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(check);
+        TimeUnit.SECONDS.sleep(1);
+        Match region4 = s.find(clefAbra);
+        s.dragDrop(region4, I);
+        s.click(max);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(check);
+        TimeUnit.SECONDS.sleep(1);
+        Match region5 = s.find(racineAbra);
+        s.dragDrop(region5, I);
+        s.click(max);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(check);
+        TimeUnit.SECONDS.sleep(1);
+        Match region6 = s.find(seveAbra);
+        s.dragDrop(region6, I);
+        s.click(max);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(check);
+        TimeUnit.SECONDS.sleep(1);
+        Match region7 = s.find(ecorceAbra);
+        s.dragDrop(region7, I);
+        s.click(max);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(check);
+
+        TimeUnit.SECONDS.sleep(1);
+        Location Z = new Location(1100, 400);
+        TimeUnit.SECONDS.sleep(1);
+        Location ZZ = new Location(400, 200);
+        s.click(ZZ);
+        Match region8 = s.find(abraGlands);
+        s.dragDrop(region8, Z);
+        TimeUnit.SECONDS.sleep(1);
+        kb.type(Key.NUM3);
+        TimeUnit.SECONDS.sleep(1);
+        kb.type(Key.NUM5);
+        TimeUnit.SECONDS.sleep(1);
+        kb.type(Key.NUM0);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(check);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(close);
+        TimeUnit.SECONDS.sleep(1);
+        s.doubleClick(popoBonta);
+        TimeUnit.SECONDS.sleep(10);
+        s.doubleClick(popoRappel);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(zaapAstrub);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(utiliser);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(selectZaapMili);
+        TimeUnit.SECONDS.sleep(1);
+        s.click(oui);
+
+
+        TimeUnit.SECONDS.sleep(2);
+        Location J = new Location(360, 270);
+        s.click(J);
+        TimeUnit.SECONDS.sleep(5);
+        Location K = new Location(360, 300);
+        s.click(K);
+        TimeUnit.SECONDS.sleep(6);
+
+        Location L = new Location(580, 580);
+        s.click(L);
+        TimeUnit.SECONDS.sleep(6);
+        Location M = new Location(720, 580);
+        s.click(M);
+        TimeUnit.SECONDS.sleep(6);
+        Location N = new Location(990, 580);
+        s.click(N);
+        TimeUnit.SECONDS.sleep(6);
+        Location O = new Location(650, 580);
+        s.click(O);
+        TimeUnit.SECONDS.sleep(6);
+        Location P = new Location(850, 580);
+        s.click(P);
+        TimeUnit.SECONDS.sleep(6);
+        Location Q = new Location(1000, 250);
+        s.click(Q);
+        TimeUnit.SECONDS.sleep(6);
+        Location R = new Location(670, 440);
+        s.click(R);
+        abraglandsIop();
+
+    }
     public static void cheminFer() throws InterruptedException, FindFailed {
         Keyboard kb = new DesktopKeyboard();
         Screen s = new Screen();
@@ -237,10 +512,10 @@ public class Main {
         TimeUnit.SECONDS.sleep(1);*/
         s.click(delock);
         TimeUnit.SECONDS.sleep(3);
-        Location G = new Location(1210, 200);
+        Location G = new Location(1100, 200);
         s.click(G);
         TimeUnit.SECONDS.sleep(1);
-        Location H = new Location(480, 200);
+        Location H = new Location(360, 200);
         s.click(H);
         TimeUnit.SECONDS.sleep(1);
         Match region1 = s.find(patteArak);
@@ -289,7 +564,7 @@ public class Main {
         s.click(close);
         TimeUnit.SECONDS.sleep(1);
         s.doubleClick(popoBonta);
-        TimeUnit.SECONDS.sleep(1);
+        TimeUnit.SECONDS.sleep(10);
         s.doubleClick(popoRappel);
         TimeUnit.SECONDS.sleep(1);
         s.click(zaapAstrub);
@@ -327,109 +602,6 @@ public class Main {
         TimeUnit.SECONDS.sleep(1);
         s.click(close);
         cheminFer();
-    }
-    public static void abraglands() throws AWTException, InterruptedException {
-        Robot robot = new Robot();
-        TimeUnit.SECONDS.sleep(3);
-        Keyboard kb = new DesktopKeyboard();
-        Screen s = new Screen();
-        Pattern goud = new Pattern("src/main/resources/img/Goud.png");
-        Pattern abraBack = new Pattern("src/main/resources/img/abraBack.png");
-        Pattern abraLeft = new Pattern("src/main/resources/img/abraLeft.png");
-        Pattern abraRight = new Pattern("src/main/resources/img/abraRight.png");
-        Pattern abraFront = new Pattern("src/main/resources/img/abraFront.png");
-        Pattern PDA = new Pattern("src/main/resources/img/PDA.png");
-        //Pattern capture = new Pattern("src/main/resources/img/capture.png");
-        Pattern goudFight = new Pattern("src/main/resources/img/goudFight.png");
-        //Pattern jugement = new Pattern("src/main/resources/img/jugement.png");
-        Pattern abraFight = new Pattern("src/main/resources/img/abraFight.png").similar((float) 0.8);
-        //Pattern fermer = new Pattern("src/main/resources/img/fermer.png");
-
-        TimeUnit.SECONDS.sleep(3);
-        for (int i = 0; i < 180; i++) {
-            try {
-                s.type(Key.F2);
-                //TimeUnit.SECONDS.sleep(1);
-                if (s.exists(abraBack) != null) {
-                    System.out.println("abraback");
-                    s.rightClick(abraBack);
-                } else if (s.exists(abraLeft) != null) {
-                    System.out.println("abraleft");
-                    s.rightClick(abraLeft);
-                } else if (s.exists(abraRight) != null) {
-                    System.out.println("abraright");
-                    s.rightClick(abraRight);
-                } else if (s.exists(abraFront) != null) {
-                    System.out.println("abrafront");
-                    s.rightClick(abraFront);
-                } else {
-                    System.out.println("ABRA NOT FOUND");
-                }
-
-                TimeUnit.SECONDS.sleep(1);
-                Location A = new Location(670, 430); //sculpteur scara
-                //Location A = new Location(900, 350); // sculpteur
-                s.click(A);
-                Location B = new Location(630, 440);
-                //Location B = new Location(730, 405);
-                s.click(B);
-
-                TimeUnit.SECONDS.sleep(1);
-                s.type(Key.F8);
-                TimeUnit.SECONDS.sleep(1);
-                s.doubleClick(PDA);
-                TimeUnit.SECONDS.sleep(1);
-                s.type(Key.F1);
-                TimeUnit.SECONDS.sleep(1);
-                s.type(Key.F8);
-                TimeUnit.SECONDS.sleep(1);
-                s.type(Key.F4);
-                s.click(abraFight);
-                TimeUnit.SECONDS.sleep(1);
-                s.type(Key.F6);
-                s.click(abraFight);
-                s.mouseMove(new Location(1200, 460));
-                s.type(Key.F1);
-                TimeUnit.SECONDS.sleep(4);
-                if (s.exists(abraFight) != null) {
-                    s.type(Key.F2);
-                    s.click(goudFight);
-                    TimeUnit.SECONDS.sleep(1);
-                    s.type(Key.F3);
-                    TimeUnit.SECONDS.sleep(1);
-                    Location C = new Location(770, 480); //scara
-                    //Location C = new Location(950, 410); //milli
-                    s.click(C);
-                    TimeUnit.SECONDS.sleep(1);
-                    s.type(Key.F6);
-                    s.click(abraFight);
-                    s.mouseMove(new Location(1200, 460));
-                    TimeUnit.SECONDS.sleep(4);
-                }
-                while (s.exists(abraFight) != null) {
-                    s.type(Key.F1);
-                    TimeUnit.SECONDS.sleep(12);
-                    s.type(Key.F6);
-                    s.click(abraFight);
-                    TimeUnit.SECONDS.sleep(1);
-                    s.type(Key.F5);
-                    s.click(abraFight);
-                    TimeUnit.SECONDS.sleep(1);
-                    s.type(Key.F5);
-                    s.click(abraFight);
-                    TimeUnit.SECONDS.sleep(1);
-                    s.mouseMove(new Location(1200, 460));
-                    s.type(Key.F1);
-                    TimeUnit.SECONDS.sleep(5);
-                }
-            } catch (FindFailed e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            s.type(Key.ESC);
-            TimeUnit.SECONDS.sleep(1);
-        }
     }
     public static void betaKamas() throws InterruptedException, FindFailed {
         boolean farming = true;
@@ -665,5 +837,8 @@ public class Main {
         TimeUnit.SECONDS.sleep(1);
         s.click(close);
         arakne();
+    }
+    public static void depodsGlands() throws InterruptedException {
+
     }
 }
